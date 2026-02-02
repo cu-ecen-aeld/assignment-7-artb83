1debe889ea7134feb33a448b54b52d4ac581e7b7
artb83 <21041434+artb83@users.noreply.github.com>
2026-02-02 13:13:33 +0200

    Modified 'hello world' to 'hello <my_username>.

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello artb83\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye artb83\n");
 }
 
 module_init(hello_init);
